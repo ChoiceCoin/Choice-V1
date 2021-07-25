8af987cb028e8fe9ae4374ff94fb44cfe6d35a8a
Choice Coin <87402354+ChoiceCoin@users.noreply.github.com>
2021-07-24 23:18:22 -0400

    Update ChoiceCoin.py

--- ChoiceCoin.py
+++ ChoiceCoin.py
@@ -47,4 +47,26 @@ def create_choice():
 	transaction_id = transaction.get_txid()
 	transaction_id = str(transaction_id)
 	print("Your transaction information is at https://testnet.algoexplorer.io/tx/" + transaction_id)
-#Sends Algo
+def init_democratic_participation():
+    parameters = algod_client.suggested_params()
+    choice_trade(fund_address,fund_key,participation_awards,500000000,asset_id,'Initial Democratic Participation Rewards')
+
+
+def democratic_awards(query,address):
+    if query == 'Evidence of electoral participation':
+        comment = 'Here is your Choice Coin Reward. \n Thanks for voting!'
+        reward_amount = 5000
+        choice_trade(fund_address, fund_key, address, reward_amount, asset_id,comment)
+    elif query == 'Letter to local legistature':
+        comment = 'Here is your Choice Coin Reward. \n Thanks for sending a letter to your local government'
+        reward_amount = 10000
+        choice_trade(fund_address, fund_key, address, reward_amount, asset_id,comment)
+    elif query == 'Running in local government':
+        comment = "Here is your Choice Coin Reward. \n Congrats on running in local government! Good Luck!"
+        reward_amount = 50000
+        choice_trade(fund_address, fund_key, address, reward_amount, asset_id,comment)
+    else:
+        comment = "Here is you Choice Coin Reward! \n Thanks for participating our democracy!"
+        reward_amount = 1000
+        choice_trade(fund_address, fund_key, address, reward_amount, asset_id,comment)
+
